0793f2ca34cc52eb60c1c2c91ff52dddcc341341
mahme003 <mahme003@doc.gold.ac.uk>
2023-12-15 16:05:24 +0000

    new

--- Database/gamedatabase.sql
+++ Database/gamedatabase.sql
@@ -14,7 +14,7 @@ USE gameDatabase;
 # Create the user which the web app will use to access the database
 DROP USER IF EXISTS 'forumapp'@'localhost';
 CREATE USER 'forumapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'qwerty';
-GRANT ALL PRIVILEGES ON gameDatabase.* TO 'forumapp'@'localhost'
+GRANT ALL PRIVILEGES ON gameDatabase.* TO 'forumapp'@'localhost';
 
 
 
