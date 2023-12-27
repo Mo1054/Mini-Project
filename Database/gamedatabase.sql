99c4f0584c04e27fbbc0077c39b2819fc6ccf54a
Mo1054 <125259181+Mo1054@users.noreply.github.com>
2023-12-27 21:53:28 +0000

    Update gamedatabase.sql

--- Database/gamedatabase.sql
+++ Database/gamedatabase.sql
@@ -1,11 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Dec 14, 2023 at 08:19 PM
--- Server version: 10.4.17-MariaDB
--- PHP Version: 7.4.15
+
 
 CREATE DATABASE gameDatabase;
 USE gameDatabase;
